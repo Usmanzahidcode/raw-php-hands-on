4cb0e497f49eb57e2bc7b44560ff11d0ad65d94a
Usman Zahid <developerusman@yahoo.com>
2025-06-02 22:28:35 +0500

    Guess the password game and ordinal suffix

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
 
-$myArray = [
-    "Usman",
-    10,
-    "10",
-    10.0,
-    [],
-    0,
-    null,
-    (new stdClass()),
-    [
-        "Usman",
-        [
-            10,
-            [
-                "10",
-                "10" + "10.11111111111111111111"
-            ]
-        ]
-    ],
-];
-var_dump($myArray);
+function generateOrdinalSuffix(int $number): string {
+    if ($number==1) {
+        $suffix = 'st';
+    } elseif ($number==2) {
+        $suffix = 'nd';
+    } elseif ($number==3) {
+        $suffix = 'rd';
+    } else {
+        $suffix = 'th';
+    }
+
+    return $suffix;
+}
+
+$password = "usman";
+$tries = 0;
+$maxTries = 5;
+
+while ($tries <= $maxTries) {
+    $tries++;
+
+    echo "Enter your password: ";
+    $input = trim(fgets(STDIN));
+//    $input = readline("Enter your password: ");
+    if ($input==$password) {
+        echo "Yay you guessed it on the $tries" . generateOrdinalSuffix($tries) . " try, yay!\n";
+        exit();
+    }
+
+    if ($tries >= $maxTries) {
+        echo "You failed!\n";
+        break;
+    }
+
+    echo "Try again!\n";
+}
