eb707c91887601f7f4ee40527fd65ccc92af3c7a
Usman Zahid <developerusman@yahoo.com>
2025-06-02 22:32:17 +0500

    fixx num practice

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <?php
 
-function generateOrdinalSuffix(int $number): string {
-    if ($number==1) {
-        $suffix = 'st';
-    } elseif ($number==2) {
-        $suffix = 'nd';
-    } elseif ($number==3) {
-        $suffix = 'rd';
+$counter = 1;
+$maxNumber = 7;
+
+while ($counter <= $maxNumber) {
+    if ($counter % 3 == 0) {
+        echo "Fizz!\n";
     } else {
-        $suffix = 'th';
+        echo "$counter\n";
     }
 
-    return $suffix;
-}
-
-$password = "usman";
-$tries = 0;
-$maxTries = 5;
-
-while ($tries <= $maxTries) {
-    $tries++;
-
-    echo "Enter your password: ";
-    $input = trim(fgets(STDIN));
-//    $input = readline("Enter your password: ");
-    if ($input==$password) {
-        echo "Yay you guessed it on the $tries" . generateOrdinalSuffix($tries) . " try, yay!\n";
-        exit();
-    }
-
-    if ($tries >= $maxTries) {
-        echo "You failed!\n";
-        break;
-    }
-
-    echo "Try again!\n";
+    $counter++;
 }
