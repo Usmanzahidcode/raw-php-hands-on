5b78a2b9619af602123a5d2f6f2e2214c62fe1d4
Usman Zahid <developerusman@yahoo.com>
2025-06-02 22:01:14 +0500

    vardump

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
-$isStudent = 1;
-var_dump($isStudent, $isStudent == true, $isStudent === true);
+$myArray = [
+    "Usman",
+    10,
+    "10",
+    10.0,
+    [],
+    0,
+    null,
+    (new stdClass()),
+    [
+        "Usman",
+        [
+            10,
+            [
+                "10",
+                "10" + "10.11111111111111111111"
+            ]
+        ]
+    ],
+];
+var_dump($myArray);
