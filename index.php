873b306dcafc14993b5ec3819a09c34a13f7567c
Usman Zahid <developerusman@yahoo.com>
2025-06-24 21:37:52 +0500

    Roll the dice do while

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-$i = 10;
-while ($i >= 1) {
-    echo "$i...\n";
+do {
+    echo "Rolling back here...\n";
     sleep(1);
-    $i--;
-}
 
-echo "Launching 🚀 \n";
+    $currentRoll = rand(1, 6);
+    if ($currentRoll===6) {
+        echo "You got a sixer 🥳 \n";
+    } else {
+        echo "You got a $currentRoll! \n";
+    }
+
+    echo "Want to roll again? (1/0)\n";
+    $redoChoice = trim(fgets(STDIN));
+} while (filter_var($redoChoice, FILTER_VALIDATE_BOOLEAN));
