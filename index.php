14b9bf21fc3bbf73c674478b6721dedbd0532b14
Usman Zahid <developerusman@yahoo.com>
2025-06-26 21:43:01 +0500

    Adding other files, difference between include, require and once

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <?php
 
-$availableAccessories = [
-    'Headphones',
-    'Charger',
-    'Sim-Tray',
-    'Sim-Ejecting-Pin',
-    'Phone-case',
-];
+include_once 'config.php';
+include_once 'config.php';
 
-echo "Here are the available accessories:\n";
-foreach ($availableAccessories as $index => $accessory) {
-    $index++;
-    echo "\t($index) $accessory\n";
-}
-
-$choice = (int) trim(fgets(STDIN));
-
-echo match($choice) {
-    1 => 'Headphones',
-    2 => 'Charger',
-    3 => 'Sim-Tray',
-    4 => 'Sim-Ejecting-Pin',
-    5 => 'Phone-case',
-    default => throw new \Exception('Unexpected match value'),
-};
+echo "Database connection successful!\n";
